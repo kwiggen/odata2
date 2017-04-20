6bd06d1d301555bff8529676c71bbb58161f5fc2
Gareth Jones <garethj@microsoft.com>
2017-04-19 22:03:50 -0700

    Working (at least for expand) odata.id version

--- odata2/Lib/CourseSerializer.cs
+++ odata2/Lib/CourseSerializer.cs
@@ -1,8 +1,12 @@
 ﻿
+using System;
 using System.Web.OData;
 using System.Web.OData.Formatter.Serialization;
 using Microsoft.OData.Core;
+using Microsoft.OData.Edm;
 using odata2.Models;
+using System.Web.OData.Query;
+using System.Reflection;
 
 namespace odata2.Lib
 {
@@ -12,6 +16,21 @@ namespace odata2.Lib
         {
         }
 
+
+        public override ODataProperty CreateStructuralProperty(IEdmStructuralProperty structuralProperty, EntityInstanceContext entityInstanceContext)
+        {
+            //if (entry.TypeName == "odata2.Models.ExternalLocation")
+            //{
+                if (structuralProperty.Name != "id")
+            {
+                return base.CreateStructuralProperty(structuralProperty, entityInstanceContext);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
         public override ODataEntry CreateEntry(SelectExpandNode selectExpandNode, EntityInstanceContext entityInstanceContext)
         {
             ODataEntry entry = base.CreateEntry(selectExpandNode, entityInstanceContext);
@@ -19,28 +38,40 @@ namespace odata2.Lib
             {
                 if (entry.TypeName == "odata2.Models.ExternalLocation")
                 {
-                    var props = entry.Properties;
-
-
-                    foreach (var prop in props)
+                    string id = string.Empty;
+                    try
                     {
-                        if (prop.Name == "id")
-                            entry.InstanceAnnotations.Add(new ODataInstanceAnnotation("kw.id",
-                                                                                      new ODataPrimitiveValue(prop.Value)));;
-
-                        
+                        // SelectExpandBinder.SelectAll<foo>
+                        object locationBinder = entityInstanceContext.EdmObject;
+                        PropertyInfo instanceProperty = locationBinder.GetType().GetProperty("Instance", BindingFlags.FlattenHierarchy | BindingFlags.Public | BindingFlags.Instance);
+                        object rawLocation = instanceProperty.GetValue(locationBinder);
+                        ExternalLocation extLocation = null;
+                        if (rawLocation != null)
+                        {
+                            extLocation = rawLocation as ExternalLocation;
+                            if (extLocation != null)
+                            {
+                                id = extLocation.Id;
+                            }
+                        }
+                        else
+                        {
+                            extLocation = entityInstanceContext.EntityInstance as ExternalLocation;
+                            if (extLocation != null)
+                            {
+                                id = extLocation.Id;
+                            }
+                        }
                     }
-                    
-
-                    //ExternalLocation location = entityInstanceContext.EntityInstance as ExternalLocation;
-                    //if (location != null)
-                    //{
-                    //entry.InstanceAnnotations.Add(new ODataInstanceAnnotation("org.northwind.search.score", 
-                    //                                                          new ODataPrimitiveValue(.Id)));
-                    // }
+                    catch (Exception e)
+                    {
+                    }
+                    if (!string.IsNullOrWhiteSpace(id))
+                    {
+                        entry.Id = new Uri(id);
+                    }
                 }
             }
-
             return entry;
         }
     }
